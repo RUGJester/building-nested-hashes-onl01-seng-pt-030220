9ce70e5542f9667d46a0b264f899521750879fca
Sean Melton <seanlape@gmail.com>
2020-03-12 19:03:23 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague =>  
+    :montague => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :hero => {
+        #your key/value pairs here
+        },
+      :hero_friends => []
+   }, 
     :capulet => {
       #your key/value pairs here!
       }}
